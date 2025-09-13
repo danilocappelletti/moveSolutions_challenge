c86459429cb78d442160ca54066bb334d9632020
danilocappelletti <danilobcappelletti@gmail.com>
2025-09-13 16:40:00 +0200

    feat: created template for project

---
+++ iot-sensors-dashboard/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 p-8">
+    <h1 class="text-3xl font-bold text-blue-600 mb-4">IoT Sensors Dashboard</h1>
+    <p class="text-gray-600">Test</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
